b5c6aa2cca859cba112ca3643d0386e3cefecb45
oldleybo <andrei.ksohelap@kuehne-nagel.com>
2023-07-29 21:44:25 +0300

    [master] added search fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
-
 import { AppComponent } from './app.component';
 import { TranscoderComponent } from './transcoder/transcoder.component';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {ErrorStateMatcher, MatNativeDateModule} from '@angular/material/core';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+
+
 
 @NgModule({
   declarations: [
@@ -13,7 +28,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/transcoder/transcoder.component.ts
+++ src/app/transcoder/transcoder.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {ErrorStateMatcher} from "@angular/material/core";
+
 
 @Component({
   selector: 'app-transcoder',
   templateUrl: './transcoder.component.html',
-  styleUrls: ['./transcoder.component.scss']
+  styleUrls: ['./transcoder.component.scss'],
 })
-export class TranscoderComponent implements OnInit{
 
-  constructor() {
-  }
+export class TranscoderComponent implements OnInit{
 
   ngOnInit(): void {
   }
